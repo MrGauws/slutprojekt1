8204570adc573ebd5ecb999ec34287ff49069ea1
MrGauws <120020363+MrGauws@users.noreply.github.com>
2024-03-06 01:05:37 +0100

    fixes and styles

--- wp-content/themes/mytheme/resources/js/checkout.js
+++ wp-content/themes/mytheme/resources/js/checkout.js
@@ -24,3 +24,29 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+$(document).ready(function() {
+    // Debounce function to prevent multiple executions
+    function debounce(func, wait, immediate) {
+        var timeout;
+        return function() {
+            var context = this, args = arguments;
+            var later = function() {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    };
+
+    // Function to move the element
+    var moveElement = debounce(function() {
+        // Move the element
+        $('#wc-stripe-payment-request-wrapper').css('margin-top', '500px');
+    }, 200); // Adjust the debounce delay as needed
+
+    // Execute the function when the document is ready
+    moveElement();
+});
